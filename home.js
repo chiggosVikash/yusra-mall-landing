aa31c8290da7c4510b58a7cd5b7163e3abde5769
Vikash Roy <chiggos.vikash@gmail.com>
2024-08-12 00:54:44 +0530

    added contact-us-page

---
+++ home.js
@@ -0,0 +1,33 @@
+// let slidesImages = document.querySelectorAll("img")
+// let prevBtn = document.querySelector("#prev")
+// let nextBtn = document.querySelector("#next")
+// let dots = document.querySelectorAll(".dot")
+
+// let currentImage = 0
+
+// nextBtn.addEventListener("click",slideNext)
+// function slideNext(){
+//     slidesImages[currentImage].style.animation = "next1 0.5s ease-in forwards"
+
+//     if(currentImage >= slidesImages.length-1){
+//         currentImage = 0
+//     }
+//     else {
+//         currentImage++
+//     }
+
+//     slidesImages[currentImage].style.animation = "next2 0.5s ease-in forwards"
+// }
+
+// prevBtn.addEventListener("click",slidePrev)
+// function slidePrev(){
+//     slidesImages[currentImage].style.animation = "prev1 0.5s ease-in forwards"
+
+//     if(currentImage == 0){
+//         currentImage = slidesImages.length-1
+//     }
+//     else {
+//         currentImage--
+//     }
+//     slidesImages[currentImage].style.animation = "prev2 0.5s ease-in forwards"
+// }
